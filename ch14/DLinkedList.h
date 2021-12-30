6d4a17cd8e10812e7e7abadbc9813fcbc2837a68
GoM1nH <>
2021-12-30 12:39:47 +0000

    211230 / for ALGraph

---
+++ ch14/DLinkedList.h
@@ -0,0 +1,35 @@
+#ifndef __D_LINKED_LIST_H__
+#define __D_LINKED_LIST_H__
+
+#define TRUE    1
+#define FALSE   0
+
+typedef int LData;
+
+typedef struct _node {
+    LData data;
+    struct _node *next;
+} Node;
+
+typedef struct _linkedlist {
+    Node *head;
+    Node *cur;
+    Node *before;
+    int numOfData;
+    int (*comp)(LData d1, LData d2);
+} LinkedList; // 연결 리스트를 생성하기 위한 구조체
+
+typedef LinkedList List;
+
+void ListInit(List *plist);
+void LInsert(List *plist, LData data);
+
+int LFirst(List *plist, LData *pdata);
+int LNext(List *plist, LData *pdata);
+
+int LRemove(List *plist);
+int LCount(List *plist)
+
+void SetSortRule(List *plist, (*comp)(LData d1, LData d2)); //정렬 기준의 지정
+
+#endif
